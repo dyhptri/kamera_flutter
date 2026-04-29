818ff51c955b8c50411cfc3c2292fcfc0b141a60
dyhptri <diyahrp10@gmail.com>
2026-04-29 17:41:20 +0700

    langkah 9

--- lib/widget/takepicture_screen.dart
+++ lib/widget/takepicture_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'displaypicture_screen.dart';
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
@@ -60,15 +61,28 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       floatingActionButton: FloatingActionButton(
         // Provide an onPressed callback.
         onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
+         // Take the Picture in a try / catch block. If anything goes wrong,
           // catch the error.
           try {
             // Ensure that the camera is initialized.
             await _initializeControllerFuture;
 
-            // Attempt to take a picture and then get the location
-            // where the image file is saved.
+            // Attempt to take a picture and get the file `image`
+            // where it was saved.
             final image = await _controller.takePicture();
+
+            if (!context.mounted) return;
+
+            // If the picture was taken, display it on a new screen.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to
+                  // the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                ),
+              ),
+            );
           } catch (e) {
             // If an error occurs, log the error to the console.
             print(e);
